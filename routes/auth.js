d101fc53181e02e48e26b384b6dcee605fc69d8e
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-11-23 00:49:05 +0530

    login user Backend added

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    registerUser
+    registerUser,
+    loginUser
 } = require("../controller/authController");
 
 router.route("/register").post(registerUser);
-
+router.route("/login").post(loginUser);
 
 module.exports = router;
