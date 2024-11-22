bb1560fb406a76cba1349e94e72125ce75a43db9
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-11-23 00:53:24 +0530

    forgot password functionality added

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const {
     registerUser,
-    loginUser
+    loginUser,
+    forgotPassword
 } = require("../controller/authController");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/password/forgot").post(forgotPassword);
+
 
 module.exports = router;
