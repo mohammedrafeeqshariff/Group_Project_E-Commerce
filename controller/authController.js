1b42a152c085c5237056d2b24bf83571e060897a
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-11-23 07:54:21 +0530

    user profile update route and controllers added

--- controller/authController.js
+++ controller/authController.js
@@ -176,3 +176,42 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
         message: "Logged out",
     });
 });
+
+
+// Update user profile   =>   /api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        address: req.body.address,
+        phone: req.body.phone,
+    };
+
+    // Update avatar
+    if (req.body.avatar !== "") {
+        const user = await User.findById(req.user.id);
+
+        const image_id = user.avatar.public_id;
+        const res = await cloudinary.v2.uploader.destroy(image_id);
+
+        const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+            folder: "avatars",
+            width: 150,
+            crop: "scale",
+        });
+
+        newUserData.avatar = {
+            public_id: result.public_id,
+            url: result.secure_url,
+        };
+    }
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+    });
+});
