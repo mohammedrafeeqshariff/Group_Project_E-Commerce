36b8de868a9ec50d739721ac5e08365fcb0546fe
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-11-23 08:17:30 +0530

    orderFetching Route added

--- routes/order.js
+++ routes/order.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    newOrder
+    newOrder,
+    myOrders,
+    getSingleOrder
 } = require("../controller/orderController");
 
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
-
-
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
 module.exports = router;
